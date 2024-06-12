54cf2898b93bd54a192a3a0800dafe76135da0eb
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-12 09:58:53 +0300

    Initial commit

---
+++ BasketballEquipment/Program.cs
@@ -0,0 +1,16 @@
+﻿ class BasketballEquipment
+ {
+     public static void Main(String[] args)
+     {
+         int annualFee = int.Parse(Console.ReadLine());
+         double basketballSneakers = annualFee - annualFee * 0.40;
+         double basketballUniform = basketballSneakers - basketballSneakers * 0.20;
+         double basketballBall = basketballUniform / 4;
+         double basketballAccessories = basketballBall / 5;
+         
+         double total_price=annualFee+basketballSneakers+basketballUniform+basketballBall+basketballAccessories;
+           Console.WriteLine(total_price);  
+         
+             
+     }
+ }
